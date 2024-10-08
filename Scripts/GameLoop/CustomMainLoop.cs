46544ea472c2567452e718dc4f4db2f0a81c28d2
Tom Douaud <tom.douaud@iut-rodez.fr>
2024-10-08 19:56:47 -0400

    ADD Hud

--- Scripts/GameLoop/CustomMainLoop.cs
+++ Scripts/GameLoop/CustomMainLoop.cs
@@ -7,6 +7,7 @@ public partial class CustomMainLoop : SceneTree
     private static CustomMainLoop _instance;
     private LevelManager _levelManager;
     private SaveManager _saveManager;
+    private CanvasLayer _canvasLayer;
     private Node _hud;
 
     public override void _Initialize()
@@ -14,15 +15,16 @@ public partial class CustomMainLoop : SceneTree
         GD.Print("Initializing CustomMainLoop");
         _levelManager = new LevelManager();
         _saveManager = new SaveManager();
+        _canvasLayer = new CanvasLayer();
         GetRoot().AddChild(_levelManager);
         GetRoot().AddChild(_saveManager);
-        
+        GetRoot().AddChild(_canvasLayer);
         // Load and add the HUD node
         var hudPacked = GD.Load<PackedScene>("res://Scenes/HUD/Hud.tscn");
         if (hudPacked != null)
         {
             _hud = hudPacked.Instantiate();
-            GetRoot().AddChild(_hud);
+            _canvasLayer.AddChild(_hud);
         }
         else
         {
